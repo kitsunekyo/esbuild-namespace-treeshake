7585a58a1e64603324ea70db37c6fe0f476e8ae0
Alex Spieslechner <alexander.spieslechner@dynatrace.com>
2024-09-05 12:13:20 +0200

    docs: add comments for ttl

--- src/tagged-template-literal.mjs
+++ src/tagged-template-literal.mjs
@@ -2,5 +2,11 @@ function tag(input) {
   return `${input}#tag`;
 }
 
-// ? treeshaking requires wrapping with IIFE and annotating
+// ❌ doesnt treeshake
+// export const tagged = tag`string`;
+
+// ❌ doesnt treeshake either
+// export const tagged = /* @__PURE__ */ tag`string`;
+
+// ✅ treeshaking requires wrapping with IIFE and annotating
 export const tagged = /* @__PURE__ */ (() => tag`string`)();
